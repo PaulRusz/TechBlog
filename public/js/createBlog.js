533a7ab958e2d25379b270fd52e6aefa7a7a9467
PaulRusz <paulruszkay@gmail.com>
2024-04-29 15:14:28 -0400

    added logout code to logout.js

---
+++ public/js/createBlog.js
@@ -0,0 +1,22 @@
+const newBlogFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#blog-title').value.trim();
+    const content = document.querySelector('#blog-content').value.trim();
+  
+    if (title && content) {
+      const response = await fetch('/api/post', {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to create blog post');
+      }
+    }
+  };
+  
+  document.querySelector('#new-blog-form').addEventListener('submit', newBlogFormHandler);
