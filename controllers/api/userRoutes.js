1f3ea36e6647f08e4b78fbe7b8eb5d0e4390c34c
PaulRusz <paulruszkay@gmail.com>
2024-04-30 19:15:18 -0400

    changed node

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -66,3 +66,4 @@ router.post('/logout', (req, res) => {
 
 
 module.exports = router;
+
--- server.js
+++ server.js
@@ -61,4 +61,3 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
-  // comment
